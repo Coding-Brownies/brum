aebbe0783fa9cc7ffda88873bc3ae7965bdcb850
just-hms <alessandroversari1@gmail.com>
2023-12-08 22:47:12 +0100

    fix: audio now starts on first input

--- cmd/brum.go
+++ cmd/brum.go
@@ -22,80 +22,92 @@ import (
 //go:embed fan.mp3
 var audio []byte
 
+func volumeFunc(val float64) float64 {
+	y := math.Exp(val/70) - 1
+	if y > 100 {
+		y = 100
+	}
+	return y
+}
+
+func sampleFunc(val float64) float64 {
+	return math.Exp(val/40) - 0.7
+}
+
 func main() {
-	embeddedReader := io.NopCloser(bytes.NewReader(audio))
-	streamer, format, err := mp3.Decode(embeddedReader)
+	// load the audio
+	streamer, format, err := mp3.Decode(
+		io.NopCloser(bytes.NewReader(audio)),
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer streamer.Close()
 
+	// create a beep.Streamer which is looped and can be resampled, and turned up and down in volume
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/30))
-
 	ctrl := &beep.Ctrl{Streamer: beep.Loop(-1, streamer)}
 	resampler := beep.ResampleRatio(4, 1, ctrl)
 	volume := &effects.Volume{Streamer: resampler, Base: 2}
 
 	read := make(chan bool)
 
+	// play asyncronously
 	go func() {
+		// wait for the first read
 		<-read
 		speaker.Play(volume)
 	}()
 
-	dest := 0.0
-	actual := 0.0
-	go func() {
-		for {
-			time.Sleep(1 * time.Millisecond)
+	destVal := 0.0
 
-			if math.Abs(dest-actual) < 0.1 {
+	// goroutine which makes followVal follow destVal
+	go func() {
+
+		followVal := destVal
+		const STEP = 0.1
+		const TIMEOUT = 1 * time.Millisecond
+
+		for {
+			time.Sleep(TIMEOUT)
+
+			// if it is near stop
+			if math.Abs(destVal-followVal) < STEP*2 {
 				continue
 			}
 
-			if dest > actual {
-				actual += 0.1
+			if destVal > followVal {
+				followVal += STEP
 			} else {
-				actual -= 0.1
+				followVal -= STEP
 			}
 
 			speaker.Lock()
-			resampler.SetRatio(math.Exp(actual/40) - 0.7)
-			volume.Volume = math.Exp(actual/30) - 1
+			resampler.SetRatio(sampleFunc(followVal))
+			volume.Volume = volumeFunc(followVal)
 			speaker.Unlock()
 		}
 	}()
 
-	first := true
-
+	isFirstRead := true
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		var (
-			text string
-			err  error
-		)
-		go func() {
-			text, err = reader.ReadString('\n')
-			if first {
-				first = false
-				read <- true
-			}
-			read <- true
-		}()
-
-		<-read
-
+		text, err := reader.ReadString('\n')
 		if errors.Is(err, io.EOF) {
 			return
 		}
-
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		dest, err = strconv.ParseFloat(strings.TrimSpace(text), 64)
+		destVal, err = strconv.ParseFloat(strings.TrimSpace(text), 64)
 		if err != nil {
 			continue
+		}
+		// if this is the first read start playing the sound
+		if isFirstRead {
+			read <- true
+			isFirstRead = false
 		}
 	}
 }
